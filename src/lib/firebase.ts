1601e5cfa318556bd2fe165320dec43f9ce07217
Wahyu Ramdani <wahyuramdani240@gmail.com>
2025-08-10 08:39:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,9 +5,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   "projectId": "tamra-vault",
@@ -25,8 +22,13 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize App Check
 if (typeof window !== 'undefined') {
-  initializeAppCheck(app, {
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  const appCheck = initializeAppCheck(app, {
     provider: new ReCaptchaV3Provider('6LemvaArAAAAAIDSl6_IU2g0lUokvUBC6K8Kwe2B'),
+
+    // Optional argument. If true, the SDK automatically refreshes App Check
+    // tokens as needed.
     isTokenAutoRefreshEnabled: true
   });
 }
